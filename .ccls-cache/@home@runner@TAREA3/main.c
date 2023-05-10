1a19600c88625247fb6b14a98bf9d996f5d44dfe
jochevipe <127540988+jochevipe@users.noreply.github.com>
2023-05-10 14:27:47 +0000

    pequeño avance en hacer el menu(switch)

--- .ccls-cache/@home@runner@TAREA3/main.c
+++ .ccls-cache/@home@runner@TAREA3/main.c
@@ -2,3 +2,98 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+
+
+void menuTexto(int *opcion) {
+
+  printf("\nElija una opcion para continuar:\n");
+  printf("\n1. Agregar tarea.\n");
+  printf("2. establecer precedencia entre tareas.\n");
+  printf("3. Mostrar tareas por hacer.\n");
+  printf("4. Marcar tarea por como completada.\n");;
+  printf("5. Deshacer última acción.\n");
+  printf("6. Importar datos desde un archivo de texto.\n");
+  printf("0. Salir del menu.\n");
+  /*
+    Usar string para comprobar que lo ingresa el usuario es un numero valido
+    para nuestro menu
+  */
+  char comprobar[2];
+  while (1) {
+    scanf("%s", comprobar);
+
+    if (isdigit(comprobar[0]) && strlen(comprobar) == 1)
+    // ver que sea un numero de un digito
+    {
+
+      *opcion = atoi(comprobar);
+      // ya cuando se comprueba se actualiza opcion
+      return;
+    }
+    printf("Entrada invalida, ingrese una opcion valida entre 0 y 9\n");
+  }
+}
+
+int main(){
+
+  int opcion;
+  Map *jugadores = createMap(is_equal_string);
+
+  printf("~~~~~~BIENVENIDO AL MENU DE JUGADOR~~~~~~\n\n");
+  while (1) {
+    menuTexto(&opcion);
+
+    switch (opcion) {
+    case 1:
+      printf("\n--------------------------------------------\n");
+      agregarTarea(jugadores);
+      printf("\n--------------------------------------------\n");
+
+      break;
+    case 2:
+      printf("\n--------------------------------------------\n");
+      establecerPrecedencia(jugadores);
+      printf("\n--------------------------------------------\n");
+      break;
+    case 3:
+      // printf("¿Que item deasea agregar?"); // lo comento pq estoy probando lo
+      // de la pila para el deshacer :)
+      printf("\n--------------------------------------------\n");
+      mostrarPorHacer(jugadores);
+      printf("\n--------------------------------------------\n");
+      break;
+    case 4:
+      printf("\n--------------------------------------------\n");
+      marcarComoCompletada(jugadores);
+      printf("\n--------------------------------------------\n");
+      break;
+    case 5:
+      printf("\n--------------------------------------------\n");
+      deshacerUltima(jugadores);
+      printf("\n--------------------------------------------\n");
+      break;
+    case 6:
+      printf("\n--------------------------------------------\n");
+      importarArchivo(jugadores);
+      printf("\n--------------------------------------------\n");
+      break;
+    case 0:
+
+      exit(EXIT_SUCCESS);
+
+    default:
+      printf("Elija una opcion del menu para continuar:\n\n");
+    }
+  }
+
+  return 0;
+}
+
+
+
+
+
+
+
+
+
