9b3cc56aafaf2054ec2c0745c07f39248ea852fa
jochevipe <127540988+jochevipe@users.noreply.github.com>
2023-05-15 02:29:08 +0000

    actualizacion del codigo y funcion 1 y 2 terminada (?)

--- main.c
+++ main.c
@@ -1,23 +1,42 @@
 #include "heap.h"
-#include "stack.h"
 #include "list.h"
+#include "stack.h"
+#include "map.h"
 #include <ctype.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct {
-  char *nombre;
-  int prioridad;
-  List *precedentes; //pueden ser varias las tareas precedentes, opino que usar una lista o un vector de strings seria bueno
-  bool completado;
-} tareaInfo;
+
+typedef struct nodo {
+  void *data;
+  int priority;
+} heapElem;
+
+typedef struct Heap {
+  heapElem *heapArray;
+  int size;
+  int capac;
+} Heap;
+
+typedef struct{ //Nodo que se guardara en el grafo 
+char *nombre;
+int prioridad;
+bool completado;
+List *nodosAdj; //Lista de tareas que la preceden
+}Nodo;
 
 typedef struct {    // stack de acciones realizadas para el deshacer acciones
   int numeroAccion; // que se realizo
-  tareaInfo tarea;  // la tarea que se realizo
+  Nodo tarea;  // la tarea que se realizo
 } accion;
+
+int is_equal_string(void *key1, void *key2) {
+  if (strcmp((char *)key1, (char *)key2) == 0)
+    return 1;
+  return 0;
+}
 
 void menuTexto(int *opcion) {
 
@@ -25,8 +44,7 @@ void menuTexto(int *opcion) {
   printf("\n1. Agregar tarea.\n");
   printf("2. establecer precedencia entre tareas.\n");
   printf("3. Mostrar tareas por hacer.\n");
-  printf("4. Marcar tarea por como completada.\n");
-  ;
+  printf("4. Marcar tarea como completada.\n");
   printf("5. Deshacer última acción.\n");
   printf("6. Importar datos desde un archivo de texto.\n");
   printf("0. Salir del menu.\n");
@@ -50,13 +68,15 @@ void menuTexto(int *opcion) {
   }
 }
 
-void agregarTarea(Heap *arreglo)
-{
+void agregarTarea(Map *grafo){
+  
   char nombre[50];
-  char precedente[50];
   int prioridad;
-  tareaInfo *tarea = malloc(sizeof(tareaInfo));
+  Nodo *tarea = (Nodo*)malloc(sizeof(Nodo)); 
+  tarea->nodosAdj = createList();
+  tarea->completado = false;
 
+  
   printf("Ingrese el nombre de la Tarea: ");
   scanf("%s", nombre);
   tarea->nombre = malloc(strlen(nombre) + 1);
@@ -66,14 +86,74 @@ void agregarTarea(Heap *arreglo)
   scanf("%i", &prioridad);
   tarea->prioridad = prioridad;
 
-  printf("Tarea agregada con éxito.\n");
+  insertMap(grafo, tarea->nombre, tarea);
+  printf("\nTarea agregada con éxito.\n");
+} //LISTO(?)
+
+void establecerPrecedencia(Map *grafo){
+  char tarea1[50];
+  char tarea2[50];
+
+  if(firstMap(grafo) == NULL){
+    puts("\nNo hay tareas guardadas");
+    return;
+  }
+  
+  printf("Ingrese el nombre de la primera tarea: ");
+  scanf("%s", tarea1);
+  
+  
+  if(searchMap(grafo, tarea1) == NULL){
+
+    puts("La tarea no se encuentra guardada.");
+    return;
+  }
+    
+  printf("\nIngrese el nombre de la segunda tarea: ");
+  scanf("%s", tarea2);
+
+  if(searchMap(grafo, tarea2) == NULL){
+
+    puts("\nLa tarea no se encuentra guardada.");
+    return;
+  }  
+  
+  Nodo *t1 = searchMap(grafo, tarea1);
+  Nodo *t2 = searchMap(grafo, tarea2);
+  
+  if(searchMap(grafo, tarea1) != NULL){
+      
+    pushBack(t1->nodosAdj, t2);
+  }
+    
+} //LISTO(?)
+
+void mostrarTareas(Map *grafo) {
+
+  List *listaOrdenada = createList();
+  Nodo *aux = firstMap(grafo);
+
+  
 }
+
+void marcarComoCompletada(Map *grafo){
+  
+}
+
+void deshacerUltima(Map *grafo){
+  
+}
+
+void importarArchivo(Map *grafo){
+  
+}
+
 
 int main() {
 
   int opcion;
   Stack *acciones;
-  Heap *arreglo = createHeap(arreglo);
+  Map *grafoTareas = createMap(is_equal_string);
 
   printf("~~~~~~BIENVENIDO AL MENU DE JUGADOR~~~~~~\n\n");
   while (1) {
@@ -82,21 +162,21 @@ int main() {
     switch (opcion) {
     case 1:
       printf("\n--------------------------------------------\n");
-      agregarTarea(arreglo);
+      agregarTarea(grafoTareas);
       printf("\n--------------------------------------------\n");
 
       break;
     case 2:
       printf("\n--------------------------------------------\n");
-      // establecerPrecedencia(jugadores);
+      establecerPrecedencia(grafoTareas);
       printf("\n--------------------------------------------\n");
+      
       break;
     case 3:
-      // printf("¿Que item deasea agregar?"); // lo comento pq estoy probando lo
-      // de la pila para el deshacer :)
       printf("\n--------------------------------------------\n");
-      // mostrarPorHacer(jugadores);
+      mostrarTareas(grafoTareas);
       printf("\n--------------------------------------------\n");
+      
       break;
     case 4:
       printf("\n--------------------------------------------\n");
